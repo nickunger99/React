985aee5cffea3729943431a8471b6d991ba3b3bf
nickunger99 <nicolas.unger@yahoo.com.br>
2023-10-03 13:04:23 -0300

    estilização

--- curso-1/src/components/Botao/index.tsx
+++ curso-1/src/components/Botao/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
+import style from "./Botao.module.scss";
 
-class Botao extends React.Component{
-    render(){
-        return(
-            <button>
-                Botão
-            </button>
-        )
-    }
+class Botao extends React.Component {
+  render() {
+    return <button
+    className={style.botao}>Botão</button>;
+  }
 }
 
 export default Botao;
