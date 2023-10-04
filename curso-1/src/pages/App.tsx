fd7202e2cd3205f0807bc06736144837ebc3cb10
nickunger99 <nicolas.unger@yahoo.com.br>
2023-10-04 08:15:53 -0300

    props e style module

--- curso-1/src/pages/App.tsx
+++ curso-1/src/pages/App.tsx
@@ -1,15 +1,14 @@
-
-import Formulario from '../components/Formulario';
-import Lista from '../components/Lista';
-import style from './App.module.scss';
-
-
+import Cronometro from "../components/Cronometro";
+import Formulario from "../components/Formulario";
+import Lista from "../components/Lista";
+import style from "./App.module.scss";
 
 function App() {
   return (
     <div className={style.AppStyle}>
-    <Formulario/>
-    <Lista/>
+      <Formulario/>
+      <Lista/>
+      <Cronometro/>
     </div>
   );
 }
