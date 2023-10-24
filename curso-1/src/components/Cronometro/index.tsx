57980c583bb9454060dd1bb63f18a02c9675605b
nickunger99 <nicolas.unger@yahoo.com.br>
2023-10-24 13:42:56 -0300

    seleção de item e tempo cronometro

--- curso-1/src/components/Cronometro/index.tsx
+++ curso-1/src/components/Cronometro/index.tsx
@@ -1,11 +1,24 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
-import style from './Cronometro.module.scss';
+import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+  useEffect(() => {
+    if (selecionado?.tempo) setTempo(tempoParaSegundos(selecionado.tempo));
+  }, [selecionado]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
--- curso-1/src/components/Lista/index.tsx
+++ curso-1/src/components/Lista/index.tsx
@@ -2,17 +2,23 @@ import style from "./Lista.module.scss";
 import Item from "./Item";
 import { ITarefa } from "../../types/tarefa";
 
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
 
-
-function Lista({tarefas}: {tarefas: ITarefa[]}) {
+function Lista({tarefas, selecionaTarefa}: Props) {
   return (
     <aside className={style.listaTarefas}>
       <h2>
         Estudos do dia
       </h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index} {...item} />
+        {tarefas.map((item)=> (
+          <Item 
+          key={item.id} {...item} 
+          selecionaTarefa={selecionaTarefa}
+          />
         ))}
       </ul>
     </aside>
