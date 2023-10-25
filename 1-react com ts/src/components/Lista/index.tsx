f00d56900571ba4a7f6e3003c3bcc8dfee8cea99
nickunger99 <nicolas.unger@yahoo.com.br>
2023-10-25 08:59:34 -0300

    padronizando pastas por cursos

---
+++ 1-react com ts/src/components/Lista/index.tsx
@@ -0,0 +1,28 @@
+import style from "./Lista.module.scss";
+import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
+
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
+
+function Lista({tarefas, selecionaTarefa}: Props) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2>
+        Estudos do dia
+      </h2>
+      <ul>
+        {tarefas.map((item)=> (
+          <Item 
+          key={item.id} {...item} 
+          selecionaTarefa={selecionaTarefa}
+          />
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
