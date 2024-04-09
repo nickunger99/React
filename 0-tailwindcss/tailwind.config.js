17bae0623ecd8a8b9560ff396752e2ecd8338bb7
nickunger99 <nicolas.unger@yahoo.com.br>
2024-04-09 15:52:16 -0300

    1 - Instalando tailwind

---
+++ 0-tailwindcss/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  safelist: [{ pattern: /^.*$/ }],
+  // ...
+};
